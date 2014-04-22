417d8d5cc17c27e16f06530d7dfbe25d10542419
Pekko Lipsanen <pekko.lipsanen@iki.fi>
2014-04-22 10:16:00 +0200

    system-specific

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+function usage {
+	echo "Usage:"
+	echo $0" [linux|mac]"
+	exit 1
+}
+
+if [[ $# < 1 ]]; then
+	usage
+fi
+
+SYSTEM=$1
 DIR=$(pwd)
+
 for f in common/*; do 
 	ln -s $DIR/$f ~/.$(basename $f);
-	# rm ~/.$(basename $f);
+done
+
+for f in $SYSTEM/*; do
+	ln -s $DIR/$f ~/.$(basename $f);
 done
 
 ln -s $DIR/common/pecko.zsh-theme $HOME/.oh-my-zsh/themes/pecko.zsh-theme
